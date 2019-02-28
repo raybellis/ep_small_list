f2e37024ec9dce9e34d2c5658d8ee946403d5564
Ray Bellis <ray@isc.org>
2019-02-28 10:31:14 +0000

    add etherpad 1.8 compatibility

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 var padManager = require('ep_etherpad-lite/node/db/PadManager');
 var async = require('../../src/node_modules/async');
+
 exports.eejsBlock_indexWrapper = function(hook_name, args, cb) {
     args.content += '<div id="small_list"></div><script src="./static/js/jquery.js"></script><script>$(function () { $("#small_list").load("./small_list"); });</script>';
 };
+
+var listAllPads = padManager.listAllPads;
+if (listAllPads.length === 0) {  // Etherpad 1.8+ detected
+    var nodeify = require('../../src/node_modules/nodeify');
+    listAllPads = function(callback) {
+        return nodeify(padManager.listAllPads(), callback);
+    }
+}
 
 exports.registerRoute = function(hook_name, args, cb) {
     args.app.get("/small_list", function(req, res) {
 
         async.waterfall([
             function(callback) {
-                padManager.listAllPads(callback);
+                listAllPads(callback);
             },
             function(data, callback) {
                 r = "<ul>";
